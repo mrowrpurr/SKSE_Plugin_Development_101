d4ffd21bb22ca992002cf1089d685824495054d4
Mrowr Purr <mrowr.purr@gmail.com>
2022-04-13 23:13:39 -0700

    Detect nearby objects and print their names

--- FormsAndObjects/plugin.cpp
+++ FormsAndObjects/plugin.cpp
@@ -1,11 +1,53 @@
 #include <SKSE/SKSE.h>
 #include <RE/C/ConsoleLog.h>
+#include <RE/P/PlayerCharacter.h>
+#include <RE/T/TES.h>
+#include <RE/T/TES.h>
+
+void PrintOutObjectReferenceInformation(RE::TESObjectREFR& ref) {
+    auto name = ref.GetName();
+    if (std::string(name).empty()) {
+        name = ref.GetBaseObject()->GetName();
+        if (std::string(name).empty()) {
+            name = ref.GetBaseObject()->GetObjectTypeName();
+        }
+    }
+    if (! std::string(name).empty()) {
+        RE::ConsoleLog::GetSingleton()->Print(std::format("Nearby Object: 0x{:x} '{}'", ref.formID, name).c_str());
+    }
+}
+
+[[noreturn]] void ListenForNearbyObjects(const std::function<void(RE::TESObjectREFR& ref)>& objectCallback, float radiusUnits = 169, int intervalMs = 4000) {
+    auto* playerRef = RE::PlayerCharacter::GetSingleton()->AsReference();
+    while (true) {
+        try {
+            RE::TES::GetSingleton()->ForEachReferenceInRange(playerRef, radiusUnits, [objectCallback](RE::TESObjectREFR& ref) {
+                try {
+                    objectCallback(ref);
+                } catch (...) {
+                    RE::ConsoleLog::GetSingleton()->Print("Could not invoke object callback");
+                }
+                return true;
+            });
+        } catch (...) {
+            RE::ConsoleLog::GetSingleton()->Print("Could not get nearby objects");
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(intervalMs));
+    }
+}
+
+void OnNearbyObjectDetected(const std::function<void(RE::TESObjectREFR& ref)>& objectCallback) {
+    std::thread objectListenerThread([objectCallback](){ ListenForNearbyObjects(objectCallback); });
+    objectListenerThread.detach();
+}
 
 extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* skse) {
     SKSE::Init(skse);
     SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* event){
         if (event->type == SKSE::MessagingInterface::kDataLoaded) {
-            RE::ConsoleLog::GetSingleton()->Print("HI I AM DIFFERENT. I am the Form/Object test!!");
+            OnNearbyObjectDetected([](RE::TESObjectREFR& ref){
+                PrintOutObjectReferenceInformation(ref);
+            });
         }
     });
     return true;
