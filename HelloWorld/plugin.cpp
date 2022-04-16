b125a991475bfe0b537b1ce4970644b8d4499eb0
Mrowr Purr <mrowr.purr@gmail.com>
2022-04-16 16:33:06 -0700

    Add a few more SKSE message events

--- HelloWorld/plugin.cpp
+++ HelloWorld/plugin.cpp
@@ -4,8 +4,18 @@
 extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* skse) {
     SKSE::Init(skse);
     SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* event){
-        if (event->type == SKSE::MessagingInterface::kDataLoaded) {
-            RE::ConsoleLog::GetSingleton()->Print("Hello Skse!");
+        if (event->type == SKSE::MessagingInterface::kPostLoad) {
+            // SKSE plugins loaded. The console is not available for printing messages until kDataLoaded
+        } else if (event->type == SKSE::MessagingInterface::kDataLoaded) {
+            RE::ConsoleLog::GetSingleton()->Print("All mods loaded, Main Menu loaded.");
+        } else if (event->type == SKSE::MessagingInterface::kNewGame) {
+            RE::ConsoleLog::GetSingleton()->Print("New game.");
+        } else if (event->type == SKSE::MessagingInterface::kSaveGame) {
+            RE::ConsoleLog::GetSingleton()->Print("Save game.");
+        } else if (event->type == SKSE::MessagingInterface::kPreLoadGame) {
+            RE::ConsoleLog::GetSingleton()->Print("Before a load game is loaded");
+        } else if (event->type == SKSE::MessagingInterface::kPostLoadGame) {
+            RE::ConsoleLog::GetSingleton()->Print("After a load game has been loaded");
         }
     });
     return true;
