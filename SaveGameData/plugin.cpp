c92db8ddf481e3b71781226975e00250e909a425
Mrowr Purr <mrowr.purr@gmail.com>
2022-04-16 08:51:21 -0700

    Playing with Save Game Data

--- SaveGameData/plugin.cpp
+++ SaveGameData/plugin.cpp
@@ -1,17 +1,48 @@
 #include <SKSE/SKSE.h>
 #include <RE/C/ConsoleLog.h>
 
-void OnSave() {
-    // todo
+struct SaveGameData {
+    int SaveGameCount = 0;
+};
+
+namespace {
+    SaveGameData SaveData;
+}
+
+void PrintSaveGameCount() {
+    RE::ConsoleLog::GetSingleton()->Print(std::format("Save game count: {}", SaveData.SaveGameCount).c_str());
+}
+
+void OnSaveData(SKSE::SerializationInterface* serialization) {
+    // Update the count
+    SaveData.SaveGameCount++;
+
+    // Save the SaveData to skse cosave
+
+}
+
+void OnLoadSaveData(SKSE::SerializationInterface* serialization) {
+    // Load the SaveData from skse cosave
+
+}
+
+void OnRevertSaveData(SKSE::SerializationInterface* serialization) {
+    SaveData.SaveGameCount = 0;
+}
+
+void InitializeSerialization() {
+    auto* serialization = SKSE::GetSerializationInterface();
+    serialization->SetUniqueID('MYID'); // Set this to a unique ID for your plugin
+    serialization->SetSaveCallback(OnSaveData);
+    serialization->SetLoadCallback(OnLoadSaveData);
+    serialization->SetRevertCallback(OnRevertSaveData);
 }
 
 extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* skse) {
     SKSE::Init(skse);
     SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* message){
-        if (message->type == SKSE::MessagingInterface::kSaveGame) {
-            RE::ConsoleLog::GetSingleton()->Print("SAVE GAME!");
-        } else if (message->type == SKSE::MessagingInterface::kPostLoadGame) {
-            RE::ConsoleLog::GetSingleton()->Print("LOAD GAME!");
+        if (message->type == SKSE::MessagingInterface::kDataLoaded) {
+            InitializeSerialization();
         }
     });
     return true;
