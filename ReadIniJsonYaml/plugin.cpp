9007fae81cc0aea96207406646ce1648af7d42a8
Mrowr Purr <mrowr.purr@gmail.com>
2022-04-23 15:48:35 -0700

    Read .yaml

--- ReadIniJsonYaml/plugin.cpp
+++ ReadIniJsonYaml/plugin.cpp
@@ -12,8 +12,27 @@ void PrintToConsole(const std::string text, const Types&... args) {
 // .json
 
 // .yaml
+#include <yaml-cpp/yaml.h>
+
+// https://stackoverflow.com/a/40903508
+std::string ReadTextFile(const std::filesystem::path& path) {
+    // Open the stream to 'lock' the file.
+    std::ifstream f(path, std::ios::in | std::ios::binary);
+
+    // Obtain the size of the file.
+    const auto sz = std::filesystem::file_size(path);
+
+    // Create a buffer.
+    std::string result(sz, '\0');
+
+    // Read the whole file into the buffer.
+    f.read(result.data(), sz);
+
+    return result;
+}
 
 void ReadIni() {
+    PrintToConsole("Read example.ini");
     auto iniPath = "Data/example.ini";
     CSimpleIni ini;
     auto loadError = ini.LoadFile(iniPath);
@@ -38,7 +57,14 @@ void ReadJson() {
 }
 
 void ReadYaml() {
-
+    PrintToConsole("Read example.yaml");
+    auto yaml = YAML::Load(ReadTextFile("Data/example.yaml"));
+    auto greeting = yaml["greeting"].Scalar();
+    // 'someNumber' will also be returned as a string.
+    // You need to parse/cast values from strings when reading from .yaml
+    auto someNumber = yaml["someNumber"].Scalar();
+    PrintToConsole("greeting: {}", greeting);
+    PrintToConsole("someNumber: {}", someNumber);
 }
 
 extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* skse) {
