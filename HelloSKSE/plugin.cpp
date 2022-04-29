c3416332942228d2605187edb184d5328a5975df
Mrowr Purr <mrowr.purr@gmail.com>
2022-04-29 09:00:29 -0700

    Re-use SKSEPlugin_Version

--- HelloSKSE/plugin.cpp
+++ HelloSKSE/plugin.cpp
@@ -1,6 +1,22 @@
 #include <SKSE/SKSE.h>
 #include <RE/C/ConsoleLog.h>
 
+extern "C" __declspec(dllexport) constinit auto SKSEPlugin_Version = [](){
+    SKSE::PluginVersionData version;
+    version.PluginName("HelloSKSE");
+    version.PluginVersion({ 0, 0, 1 });
+    version.CompatibleVersions({ SKSE::RUNTIME_LATEST });
+    version.UsesAddressLibrary(true);
+    return version;
+}();
+
+extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface*, SKSE::PluginInfo* info) {
+    info->name = SKSEPlugin_Version.pluginName;
+    info->version = SKSEPlugin_Version.pluginVersion;
+    info->infoVersion = SKSE::PluginInfo::kVersion;
+    return true;
+}
+
 extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* skse) {
     SKSE::Init(skse);
     SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* event){
@@ -20,19 +36,3 @@ extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadIn
     });
     return true;
 }
-
-extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface*, SKSE::PluginInfo* info) {
-    info->name = "HelloSKSE";
-    info->infoVersion = SKSE::PluginInfo::kVersion;
-    info->version = 1;
-    return true;
-}
-
-extern "C" __declspec(dllexport) constinit auto SKSEPlugin_Version = [](){
-    SKSE::PluginVersionData version;
-    version.PluginName("HelloSKSE");
-    version.PluginVersion({ 0, 0, 1 });
-    version.CompatibleVersions({ SKSE::RUNTIME_LATEST });
-    version.UsesAddressLibrary(true);
-    return version;
-}();
